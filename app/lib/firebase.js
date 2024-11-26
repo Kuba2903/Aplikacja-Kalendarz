26ece722bc18e2650e2bf2249e7e555b42252e4f
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-11-26 16:06:17 +0100

    a

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCmaDOrO1Vvs88w_j4eaapK_UnJFO7HZT0",
